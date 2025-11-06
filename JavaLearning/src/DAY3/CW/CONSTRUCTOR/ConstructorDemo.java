f1fd587b1c4ad71fbe07a254a4f1ce396c53e4f7
dhikshisan7 <dhikshisan@gmail.com>
2025-11-06 20:58:49 +0530

    added ConstructorDemo file

--- JavaLearning/src/DAY3/CW/CONSTRUCTOR/ConstructorDemo.java
+++ JavaLearning/src/DAY3/CW/CONSTRUCTOR/ConstructorDemo.java
@@ -1,4 +1,36 @@
 package DAY3.CW.CONSTRUCTOR;
 
+class Students{
+    String name;
+    int rno;
+    String dept;
+    static String college;
+    //    Parameterized Constructor
+    public Students(String name,int rno,String dept,String college){
+        this.name=name;
+        this.rno=rno;
+        this.dept=dept;
+        this.college=college;
+    }
+    //    No-args Constructor
+    public Students(){
+
+    }
+    public void display(){
+        System.out.println("Name: "+name+"\nRno: "+rno+"\nDept: "+dept+"\nCollege Name: "+college);
+    }
+}
 public class ConstructorDemo {
+    public static void main(String[] args) {
+        Students.college="MSAJCE";
+        Students s1=new Students("Dhikshi",11,"CSE","MSAJCE");
+        s1.display();
+        System.out.println("----------------------");
+        Students s2=new Students();
+        s2.name="Pooja";
+        s2.rno=12;
+        s2.dept="CSE";
+        s2.display();
+
+    }
 }
