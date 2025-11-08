c3989a1125eff39e5fc260982f07127c8ba3de05
dhikshisan7 <dhikshisan@gmail.com>
2025-11-08 09:12:04 +0530

    added DAY8 files

---
+++ JavaLearning/src/DAY8/THREAD/EXTENDTHREAD/ByExtendingThreadClass.java
@@ -0,0 +1,14 @@
+package DAY8.THREAD.EXTENDTHREAD;
+
+class MyThread extends Thread{
+    public void run(){
+        System.out.println("MyThread is running: "+Thread.currentThread().getName());
+    }
+}
+public class ByExtendingThreadClass {
+    public static void main(String[] args) {
+        MyThread t1=new MyThread();
+        t1.start();
+    }
+}
+
