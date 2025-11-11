1fdeec0b2ab78f27b43ab1774a4ff947a878bd4a
dhikshisan7 <dhikshisan@gmail.com>
2025-11-11 21:03:58 +0530

    added MINIPROJECT2 files

---
+++ JavaLearning/src/MINIPROJECT2/LogTask.java
@@ -0,0 +1,28 @@
+package MINIPROJECT2;
+
+import java.util.*;
+import java.util.concurrent.Callable;
+
+public class LogTask implements Callable<Map<String, Integer>> {
+    private List<String> logLines;  // fake log file content
+
+    public LogTask(List<String> logLines) {
+        this.logLines = logLines;
+    }
+
+    @Override
+    public Map<String, Integer> call() {
+        Map<String, Integer> keywordCount = new HashMap<>();
+        for (String line : logLines) {
+            line = line.toLowerCase();
+            if (line.contains("error"))
+                keywordCount.merge("error", 1, Integer::sum);
+            if (line.contains("warning"))
+                keywordCount.merge("warning", 1, Integer::sum);
+            if (line.contains("failed"))
+                keywordCount.merge("failed", 1, Integer::sum);
+        }
+        System.out.println(Thread.currentThread().getName() + " analyzed file");
+        return keywordCount;
+    }
+}
